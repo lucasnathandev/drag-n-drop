ac9a2e0c708337aff74c913d131baa48695e1469
Solium Dev <lucas.develop.prog@gmail.com>
2022-08-10 21:49:42 -0300

    :sparkles: Project finished. Ready for deploy.

---
+++ script.js
@@ -0,0 +1,42 @@
+const fill = document.querySelector(".fill")
+const empties = document.querySelectorAll(".empty")
+
+function dragStart() {
+  this.className += " hold"
+  setTimeout(() => (this.className = "invisible"), 0)
+}
+function dragEnd() {
+  this.className = "fill"
+}
+function dragOver(e) {
+  e.preventDefault()
+}
+function dragEnter(e) {
+  e.preventDefault()
+  this.className += " hovered"
+}
+function dragLeave() {
+  this.className = "empty"
+}
+function dragDrop() {
+  this.className = "empty"
+  this.append(fill)
+}
+
+function main() {
+  fill.addEventListener("dragstart", dragStart)
+  fill.addEventListener("dragend", dragEnd)
+
+  empties.forEach((empty) => {
+    empty.addEventListener("dragover", dragOver)
+    empty.addEventListener("dragenter", dragEnter)
+    empty.addEventListener("dragleave", dragLeave)
+    empty.addEventListener("drop", dragDrop)
+  })
+  // fill.addEventListener("dragover", dragOver)
+  // fill.addEventListener("dragenter", dragEnter)
+  // fill.addEventListener("dragleave", dragLeave)
+  // fill.addEventListener("dragdrop", dragDrop)
+}
+
+main()
